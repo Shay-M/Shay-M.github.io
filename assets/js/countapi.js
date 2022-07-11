bdfe5fd15f21e37c6337ad97a34bacedce33f547
“ShayM” <shay1mualem@gmail.com>
2022-07-11 19:06:38 +0300

    upd

--- assets/js/countapi.js
+++ assets/js/countapi.js
@@ -1,8 +1,20 @@
 // https://api.countapi.xyz/create?namespace=shay-m.github.io&enable_reset=1
+
+// function websiteVisits(response) {
+//   //   document.getElementById('visits').innerText = response.value;
+//   document.getSelection('#visits').textContent = response.value;
+//   console.log('>> ' + response.value);
+// }
+
+// https://api.countapi.xyz/get/mysite.com/test
+// function websiteVisits(response) {
+//     //   document.getElementById('visits').innerText = response.value;
+//     document.getSelection('#visits').textContent = response.value;
+//     console.log('>> ' + response.value);
+//   }
+
 import countapi from 'countapi-js';
 
-function websiteVisits(response) {
-  //   document.getElementById('visits').innerText = response.value;
-  document.getSelection('visits').textContent = response.value;
-  console.log('>> ' + response.value);
-}
+countapi.visits().then((result) => {
+  console.log(result.value);
+});

d6d4122ae8534f5313ee2b7724fdf8a2e758ed6f
“ShayM” <shay1mualem@gmail.com>
2022-07-11 18:55:22 +0300

    update

--- assets/js/countapi.js
+++ assets/js/countapi.js
@@ -1,7 +1,8 @@
 // https://api.countapi.xyz/create?namespace=shay-m.github.io&enable_reset=1
+import countapi from 'countapi-js';
 
 function websiteVisits(response) {
   //   document.getElementById('visits').innerText = response.value;
-  document.getSelection('#visits').textContent = response.value;
+  document.getSelection('visits').textContent = response.value;
   console.log('>> ' + response.value);
 }

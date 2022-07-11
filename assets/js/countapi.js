fd93ec7ee1205700549c5ec2e165c323b751681b
“ShayM” <shay1mualem@gmail.com>
2022-07-11 17:21:22 +0300

    add count web

---
+++ assets/js/countapi.js
@@ -0,0 +1,6 @@
+// https://api.countapi.xyz/create?namespace=shay-m.github.io&enable_reset=1
+
+function websiteVisits(response) {
+  //   document.getElementById('visits').innerText = response.value;
+  document.getSelection('#visits').textContent = response.value;
+}
